e64733e632f73a9f9bd0e7df26654f958a769081
Bongyong Song <31605430+manadoona@users.noreply.github.com>
2018-04-12 12:19:31 +0900

    1. 클래스화 2. 메인에서 호출

--- lottoanalyzer.py
+++ lottoanalyzer.py
@@ -1,38 +1,45 @@
 import openpyxl
 
-book = openpyxl.load_workbook('lotto.xlsx')
-sheet = book.active
-rows = sheet.rows
+class LottoAnalyzer():
+    def setData(self, filename):
+        book = openpyxl.load_workbook(filename)
+        sheet = book.active
+        data_ragne = sheet['B4':'T804']
 
-lottoInfo = {}
-data_ragne = sheet['B4':'T804']
-for row in data_ragne:
-    values = []
-    for cell in row:
-        values.append(cell.value)
+        lottoInfo = {}
+        for row in data_ragne:
+            values = []
+            for cell in row:
+                values.append(cell.value)
 
-    try:
-        lottodata = {}
-        lottodata['추첨일'] = values[1]
-        for i in range(5):
-            winnerdata = {}
-            winnerdata['당첨자수'] = values[i*2 + 2]
-            winnerdata['당첨금액'] = values[i*2 + 3]
-            lottodata[i+1] = winnerdata
-        lottodata['당첨번호'] = values[12:19]
-        lottoInfo[values[0]] = lottodata
-    except:
-        lottodata = {}
-        lottodata['추첨일'] = values[1]
-        for i in range(5):
-            winnerdata = {}
-            winnerdata['당첨자수'] = values[i*2 + 2]
-            winnerdata['당첨금액'] = values[i*2 + 3]
-            lottodata[i+1] = winnerdata
-        lottodata['당첨번호'] = values[12:19]
-        lottoInfo[values[0]] = lottodata
+            try:
+                lottodata = {}
+                lottodata['추첨일'] = values[1]
+                for i in range(5):
+                    winnerdata = {}
+                    winnerdata['당첨자수'] = values[i*2 + 2]
+                    winnerdata['당첨금액'] = values[i*2 + 3]
+                    lottodata[i+1] = winnerdata
+                lottodata['당첨번호'] = values[12:19]
+                lottoInfo[values[0]] = lottodata
+            except:
+                lottodata = {}
+                lottodata['추첨일'] = values[1]
+                for i in range(5):
+                    winnerdata = {}
+                    winnerdata['당첨자수'] = values[i*2 + 2]
+                    winnerdata['당첨금액'] = values[i*2 + 3]
+                    lottodata[i+1] = winnerdata
+                lottodata['당첨번호'] = values[12:19]
+                lottoInfo[values[0]] = lottodata
 
-for index, info in lottoInfo.items():
-    print(index, info)
+        return lottoInfo
 
+
+if __name__ == "__main__":
+    lottoAnalyzer = LottoAnalyzer()
+    result = lottoAnalyzer.setData("lotto.xlsx")
+
+    for index, info in result.items():
+        print(index, info)
 
