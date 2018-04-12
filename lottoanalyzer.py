1fbaa7827d3d1ed27b2728c83f92410e1440e685
Bongyong Song <31605430+manadoona@users.noreply.github.com>
2018-04-12 11:16:11 +0900

    실제 필요한 데이터 영역 추출

--- lottoanalyzer.py
+++ lottoanalyzer.py
@@ -4,8 +4,13 @@ book = openpyxl.load_workbook('lotto.xlsx')
 sheet = book.active
 rows = sheet.rows
 
-for row in rows:
+data_ragne = sheet['B4':'T804']
+for row in data_ragne:
     values = []
     for cell in row:
         values.append(cell.value)
     print(values)
+
+
+
+
