16340df522916a9bf80b69bc693fc6cef8e84a05
Bongyong Song <31605430+manadoona@users.noreply.github.com>
2018-04-12 10:33:24 +0900

    first upload
    first upload

---
+++ lottoanalyzer.py
@@ -0,0 +1,13 @@
+import openpyxl
+
+book = openpyxl.load_workbook('lotto.xlsx')
+sheet = book.active
+rows = sheet.rows
+
+for row in rows:
+    values = []
+    for cell in row:
+        values.append(cell.value)
+    print(values)
+
+print("OK")
